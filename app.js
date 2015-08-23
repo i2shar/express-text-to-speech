962b9c35cb827b54dddd2b760d3e8189190abd73
Tushar Inamdar <tushar@waterlinedata.com>
2015-08-22 19:38:59 -0700

    Directory structure cleanup Scripts cleanup and simplification Bug fixing

--- app.js
+++ app.js
@@ -1,88 +1,68 @@
 var express = require('express');
 var app = express();
 var child_process = require('child_process');
-var utils = require('./util');
-var config = require('./config');
+var basicAuth = require('./lib/auth').basicAuth;
+var config = require('./conf/config');
 
-var winston = require('winston');
-var logger = new winston.Logger({
-    transports: [
-        new winston.transports.File({
-            timestamp: function () {
-                var date = new Date();
-                return date.getFullYear() + '-' +
-                    (date.getMonth() + 1) + '-' +
-                    date.getDate() + ' ' + date.toLocaleTimeString();
-            },
-            formatter: function (options) {
-                return options.timestamp() + ' ' + options.level.toUpperCase() + ' | ' + (undefined !== options.message ? options.message : '') +
-                    (options.meta && Object.keys(options.meta).length ? '\n\t' + JSON.stringify(options.meta) : '' );
-            },
-            filename: 'webserver.log',
-            handleExceptions: true,
-            json: false
-        })
-    ],
-    exitOnError: false
+var logger = require('./lib/winston');
+
+app.use(basicAuth(config.basicAuth.username, config.basicAuth.password));
+
+app.use(function(req, res, next) {
+    next();
+    logRequest(req, res);
+    res.type('text/plain; charset=utf-8');
+    res.set("Connection", "close");
+    res.status(200).send("OK");
 });
-
-app.use(utils.basicAuth(config.username, config.password));
 
 app.get('/speak/:message/:lang?', function (req, res) {
     var message = req.params.message;
     var language = req.params.lang || 'en';
 
-    logRequest(req);
-
-    playSound('soft_chime');
+    playSound('soft_chime.mp3');
     speak(language, message);
-
-    logger.info('------------------------------');
-
-    res.set("Connection", "close");
-    res.status(200).send("OK");
 });
 
 app.get('/speakOnly/:message/:lang?', function (req, res) {
     var message = req.params.message;
     var language = req.params.lang || 'en';
 
-    logRequest(req);
-
     speak(language, message);
-    logger.info('------------------------------');
-    res.set("Connection", "close");
-    res.status(200).send("OK");
 });
 
 app.get('/play/:file', function (req, res) {
     var file = req.params.file;
-    logRequest(req);
     if (file) {
-        playSound('soft_chime');
         playSound(file);
     }
-    res.status(200).send("OK");
+});
+
+app.use(function(err, req, res, next) {
+    logger.error(err.stack);
+    res.status(500).send('Something broke!');
 });
 
 function playSound(file) {
-    execProcess(config.playCommand + ' sounds/' + file + '.mp3');
+    execProcess(config.playCommand + ' sounds/' + file);
 }
 
 function speak(language, message) {
+
+    // message needs to be passed in quotes, so that quotes is messages are handled correctly
     switch (language) {
         case 'hi':
-            execProcess(config.speakHindiCommand + ' ' + message);
+            execProcess(config.speakHindiCommand + " \"" + message + "\"");
             break;
         case 'en':
         default:
-            execProcess(config.speakCommand + ' ' + message);
+            execProcess(config.speakCommand + " \"" + message + "\"");
             break;
     }
 }
 
 function logRequest(req) {
-    logger.info("Request IP: " + req.ip + ", method: " + req.method + " " + req.url + ", params: ", req.params);
+    logger.info("Request IP: " + req.ip + ", user: " + req.user + ", method: " + req.method + " " + req.url + ", params: ", req.params);
 }
 
 function execProcess(command) {
